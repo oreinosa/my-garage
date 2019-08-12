ead974d82566d4cd36d8315b33382ff89b856ac5
Oscar Reinosa <oscar.reinosa96@gmail.com>
2019-08-12 11:59:55 -0600

    added login and signup routes

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "sign-up", component: SignUpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, SignUpComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
